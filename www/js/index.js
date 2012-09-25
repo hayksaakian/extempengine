3b35db568cc74a88775ae55548cebc029ada3ff8
hayksaakian <fakest.id.ever@gmail.com>
2012-09-24 19:25:41 -0700

    things

--- www/js/index.js
+++ www/js/index.js
@@ -67,9 +67,9 @@ var app = {
                         lyo.find("#title").text(cur_a["title"]);
                         lyo.id = cur_a["_id"];
                         lyo.find("#body").text(cur_a["body"]);
-                        lyo.find("#published_at").contents(cur_a["published_at"]);
-                        lyo.find("#author").contents(cur_a["author"]);
-                        lyo.find("#source").contents("paper_id:"+cur_a["paper_id"]+"| "+cur_a["url"]);
+                        lyo.find("#published_at").contents(cur_a["published_at"] + " | ");
+                        lyo.find("#author").contents(cur_a["author"] + " | ");
+                        lyo.find("#source").contents("paper_id:"+cur_a["paper_id"]+" | "+cur_a["url"]);
                     }
                 });
             }
