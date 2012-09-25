d8154c29dae83a60c51b69103fa63d8da2b7b5bb
hayksaakian <fakest.id.ever@gmail.com>
2012-09-24 20:50:33 -0700

    touchdown

--- www/js/index.js
+++ www/js/index.js
@@ -56,9 +56,9 @@ var app = {
                         lyo.find("#title").text(cur_a["title"]);
                         lyo.id = cur_a["_id"];
                         lyo.find("#body").text(cur_a["body"]);
-                        lyo.find("#published_at").append(cur_a["published_at"] + " | ");
-                        lyo.find("#author").append(cur_a["author"] + " | ");
-                        lyo.find("#source").append("paper_id:"+cur_a["paper_id"]+" | "+cur_a["url"]);
+                        lyo.find("#published_at").append(" "+cur_a["published_at"]);
+                        lyo.find("#author").append(" "+cur_a["author"]);
+                        lyo.find("#source").append(" paper_id:"+cur_a["paper_id"]+" | "+cur_a["url"]);
                     }
                 });
             }
@@ -188,8 +188,9 @@ var app = {
             function make_article_layout(){
                 var template = $("#article_template");
                 var cont = $("#beer_list");
-                var n = template.clone();
+                var n = template.contents().clone();
                 n.appendTo(cont);
+                n.show();
                 return n;                
             }
             $("#clear_db").click(function(e){
