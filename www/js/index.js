be4caca7761af5b6f0f904c4e683c6a2a306ce34
hayksaakian <fakest.id.ever@gmail.com>
2012-10-03 19:41:25 -0700

    regex search

--- www/js/index.js
+++ www/js/index.js
@@ -53,11 +53,11 @@ var app = {
                     {
                         var lyo = make_article_layout();
                         cur_a = arrBeers[i].value;
-                        lyo.find("#title").text(" "+cur_a["title"]);
                         lyo.id = cur_a["_id"];
                         //lyo.find("#body").text(cur_a["body"]);
                         lyo.find("#published_at").append(" "+cur_a["published_at"]);
                         lyo.find("#author").append(" "+cur_a["author"]);
+                        lyo.find("#title").append(" "+cur_a["title"]);
                         lyo.find("#source").append(" paper_id:"+cur_a["paper_id"]+" | "+cur_a["url"]);
                     }
                 });
